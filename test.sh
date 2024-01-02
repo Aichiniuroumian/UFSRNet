6fb45f1310e42d8c843e68f9dab2093e0c813fd1
Aichiniuroumian <77386057+Aichiniuroumian@users.noreply.github.com>
2024-01-02 16:56:51 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,25 +17,3 @@ python test.py --gpus 1 --model ufsrnet --name UFSRNet_sy92 \
 # ----------------- calculate PSNR/SSIM scores ----------------------------------
 python psnr_ssim.py
 # ------------------------------------------------------------------------------- 
-
-# ================================================================================
-# Test SPARNetHD for aligned images
-# ================================================================================
-
-python test.py --gpus 1 --model sparnethd --name SPARNetHD_V4_Attn2D \
-    --res_depth 10 --att_name spar --Gnorm 'in' \
-    --load_size 512 --dataset_name single --dataroot test_dirs/CelebA-TestN/ \
-    --pretrain_model_path ./pretrain_models/SPARNetHD_V4_Attn2D_net_H-epoch10.pth \
-    --save_as_dir results_CelebA-TestN/SPARNetHD_V4_Attn2D/
-
-python test.py --gpus 1 --model sparnethd --name SPARNetHD_V4_Attn3D \
-    --res_depth 10 --att_name spar3d --Gnorm 'in' \
-    --load_size 512 --dataset_name single --dataroot test_dirs/CelebA-TestN/ \
-    --pretrain_model_path ./pretrain_models/SPARNetHD_V4_Attn3D_net_H-epoch10.pth \
-    --save_as_dir results_CelebA-TestN/SPARNetHD_V4_Attn3D/
-
-# ----------------- calculate FID scores ----------------------------------
-python -m pytorch_fid results_CelebA-TestN/SPARNetHD_V4_Attn2D/ test_dirs/CelebAHQ-Test-HR 
-python -m pytorch_fid results_CelebA-TestN/SPARNetHD_V4_Attn3D/ test_dirs/CelebAHQ-Test-HR 
-# ------------------------------------------------------------------------------- 
-
